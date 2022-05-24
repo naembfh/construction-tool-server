d5846b31d70936c610604969e6552563a2baf224
Mehedi Nayem <naembfh@gmail.com>
2022-05-25 02:22:18 +0800

    add review

--- index.js
+++ index.js
@@ -3,16 +3,67 @@ const app = express()
 const port =process.env.PORT || 5000;
 const cors = require('cors');
 require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 app.use(cors());
 app.use(express.json())
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yuavs.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+  try{
+    await client.connect();
+    const productCollection=client.db('construction_tools').collection('products');
+    const orderCollection=client.db('construction_tools').collection('orders');
+    const reviewCollection=client.db('construction_tools').collection('reviews');
+
+    app.get('/product',async(req,res)=>{
+        const query={}
+        const cursor=productCollection.find(query);
+        const products=await cursor.toArray()
+        res.send(products)
+    })
+
+    app.get('/product/:id',async(req,res)=>{
+        const id=req.params.id;
+        const query={_id:ObjectId(id)};
+        const product=await productCollection.findOne(query);
+        res.send(product);
+    })
+app.post('/orders',async(req,res)=>{
+    const newOrder=req.body;
+    const result=await orderCollection.insertOne(newOrder);
+    res.send(result);
+})
+
+app.get('/orders' ,async(req,res)=>{
+const email=req.query;
+console.log(email)
+const myItem=await orderCollection.find(email).toArray()
+res.send(myItem)
+})
+
+app.post('/reviews',async(req,res)=>{
+  const newReview=req.body;
+  const review=await reviewCollection.insertOne(newReview);
+  res.send(review)
+})
+  }  
+  finally{
+
+  }
+}
+run().catch(console.dir);
+
+
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
-  
-  app.listen(port, () => {
-    console.log(`Manufacturer app listening on port ${port}`)
-  })
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Manufacturer app listening on port ${port}`)
+})
