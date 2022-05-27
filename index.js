a9a5868162ddc96415efd0120f7fc6c9a8e23b1a
Mehedi Nayem <naembfh@gmail.com>
2022-05-27 08:15:50 +0800

    manage all ordeers

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port =process.env.PORT || 5000;
 const cors = require('cors');
 require('dotenv').config()
 var jwt = require('jsonwebtoken');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const { MongoClient, ServerApiVersion, ObjectId, Admin } = require('mongodb');
 const verify = require('jsonwebtoken/verify');
@@ -11,6 +12,14 @@ const verify = require('jsonwebtoken/verify');
 
 app.use(cors());
 app.use(express.json())
+
+//
+ 
+// 
+
+
+
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yuavs.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -22,7 +31,7 @@ function verifyJWT(req,res,next){
     return res.status(403).send('can not access')
   }
   const token=authHeader.split(' ')[1]
-  console.log(token)
+  // console.log(token)
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded) {
     if(err){
       return res.status(401).send('can not access')
@@ -39,6 +48,23 @@ async function run(){
     const orderCollection=client.db('construction_tools').collection('orders');
     const reviewCollection=client.db('construction_tools').collection('reviews');
     const userCollection=client.db('construction_tools').collection('users');
+
+// payment
+app.post('/create-payment-intent',async(req,res)=>{
+  const service=req.body;
+  console.log(service)
+  const price=service.price;
+  const amount=price*100;
+  
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount:amount,
+    currency:'usd',
+payment_method_types:['card']
+  })
+  res.send({clientSecret:paymentIntent.client_secret})
+})
+
+
 
 // products
 
@@ -73,6 +99,18 @@ const email=req.query;
 // console.log(email)
 const myItem=await orderCollection.find(email).toArray()
 res.send(myItem)
+})
+app.get('/allOrders',async(req,res)=>{
+  const orders= await orderCollection.find().toArray()
+  res.send(orders)
+})
+
+app.get('/orders/:id',async(req,res)=>{
+  const id=req.params.id;
+  // console.log(id)
+  const query={_id:ObjectId(id)}
+  const result=await orderCollection.findOne(query)
+  res.send(result)
 })
 // reviews
 app.post('/reviews',async(req,res)=>{
